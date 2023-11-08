e362d2316bd81f3bc756e29a3da0cd722aae79e2
Richard Laychock <rickylaychock155@gmail.com>
2023-11-07 20:21:09 -0500

    Added button click function

--- javascript.js
+++ javascript.js
@@ -1,26 +1,18 @@
+const validOperators = ["+", "*", "/", "-"];
 let currNum1 = "";
-let currNum2 = "";
+let result = 0;
 let currOperator = "";
 
 
 const displayValue = document.getElementById("displayScreen");
 const buttons = document.querySelectorAll("button");
 
+
 buttons.forEach((button) => {
     button.addEventListener("click", function() {
-        let value = button.textContent;
-
-        if(!isNaN(value) || value ===".") {
-            currNum1 += value;
-            setDisplay(currNum1);
-        } else if(value === "CLEAR"){
-            clear();
-            
-        }
-    }); 
+        buttonClicked(button.textContent);    
+    });
 });
-
-
 
 
 function add(num1, num2) {
@@ -48,7 +40,16 @@ function divide(num1,num2) {
 }
 
 function operate(num1, num2, operate) {
-
+    switch(operate) {
+        case "+": 
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+    }
 }
 
 
@@ -60,7 +61,28 @@ function setDisplay(item) {
 
 function clear() {
     currNum1 = "";
-    currNum2 = "";
+    result = 0;
     currOperator = "";
     displayValue.value = "";
+}
+
+
+function buttonClicked(value) {
+
+    if(value === "CLEAR") {clear(); return;} 
+
+    //If value is a number, add it to current number
+    if(!isNaN(value) || value ===".") {
+        currNum1 += value;
+        setDisplay(currNum1);
+
+    }  else if (validOperators.includes(value)){
+        currOperator = value;
+        setDisplay(currOperator);
+        result = currNum1;
+        currNum1 = "";
+    } else {
+        if(currNum1==="") {return};
+        setDisplay(operate(parseInt(result),parseInt(currNum1),currOperator));
+    }
 }
