d5f9e91a62cc4d373bd39c29d49f4f4f5c00187f
Richard Laychock <rickylaychock155@gmail.com>
2023-11-07 19:11:56 -0500

    Added Display window onclick for buttons and clear button

--- javascript.js
+++ javascript.js
@@ -1,23 +1,43 @@
-let currNum1
-let currNum2
-let currOperator 
+let currNum1 = "";
+let currNum2 = "";
+let currOperator = "";
 
-//1
+
+const displayValue = document.getElementById("displayScreen");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        let value = button.textContent;
+
+        if(!isNaN(value) || value ===".") {
+            currNum1 += value;
+            setDisplay(currNum1);
+        } else if(value === "CLEAR"){
+            clear();
+            
+        }
+    }); 
+});
+
+
+
+
 function add(num1, num2) {
     return num1+num2;
 }
 
-//2
+
 function subtract(num1, num2) {
     return num1-num2;
 }
 
-//3
+
 function multiply(num1,num2) {
     return num1*num2;
 }
 
-//4
+
 function divide(num1,num2) {
     if(num2 === 0) {
         return "idiot";
@@ -28,22 +48,19 @@ function divide(num1,num2) {
 }
 
 function operate(num1, num2, operate) {
-   switch (operate) {
-        case 1:
-            return add(num1,num2);
-            break;
-        case 2:
-            return subtract(num1,num2);
-            break;
-        case 3:
-            return multiply(num1,num2);
-            break;
-        case 4: 
-            return divide(num1,num2);
-            break;
-   } 
+
 }
 
 
 
+function setDisplay(item) {
+    displayValue.value = item;
+}
 
+
+function clear() {
+    currNum1 = "";
+    currNum2 = "";
+    currOperator = "";
+    displayValue.value = "";
+}
